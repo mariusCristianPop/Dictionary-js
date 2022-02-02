34053920773bf3ec91f69ac8ef34dca3573fdee7
Marius Pop <30757467+mariusCristianPop@users.noreply.github.com>
2022-02-02 20:59:55 +0100

    validation and cleaning up

--- js/myScript.js
+++ js/myScript.js
@@ -3,33 +3,47 @@ var firstInputField;
 var secondInputField;
 // append word to list
 function appendWord() {
-    getValueOfInput();
-    if (myWords.indexOf(firstInputField) == -1) {
+    getInputValue();
+    if (validateInput(firstInputField)) {
         myWords.push(firstInputField);
     } else {
-        console.log("cuvantul exista deja");
+        console.log("cuvantul exista sau nu este in formatul cerut");
     }
-    
     console.log(myWords);
-    
 }
 
 // search word trough list
 function searchWord() {
-    getValueOfInput();
-    if (myWords.indexOf(secondInputField) != -1) {
-        console.log("se gaseste in lista");
-        
-    } else {
+    getInputValue();
+    if (validateInput(secondInputField)) {
+        console.log("se gaseste in lista");    
+    }
+     else {
         console.log("nu se gaseste in lista");
-        document.getElementById("userWordSearch").value = null;
     }
 }
 
-function getValueOfInput() {
+function getInputValue() {
     firstInputField = document.getElementById("userWordInput").value;
     secondInputField = document.getElementById("userWordSearch").value;
+    clearInputValues();
+}
+
+function clearInputValues() {
     document.getElementById("userWordSearch").value = null;
     document.getElementById("userWordInput").value = null;
 }
 
+function validateInput(input) {
+    var r = /\s+/;
+    if(input.match(r)) {
+        console.log("gasit spatiu");
+        return false;
+    }
+    var r = /\d+/;
+    if (input.match(r)) {
+        console.log("gasit cifre");
+        return false;
+    }
+    return true;
+}
